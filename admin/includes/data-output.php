5a9d43fb8cf690664ab6f7509d93462331301f35
Beee <info@berryplasman.com>
2024-09-27 11:12:02 +0200

    Move up + add scroll class

--- admin/includes/data-output.php
+++ admin/includes/data-output.php
@@ -1,9 +1,11 @@
+<?php
+    $amount_rows  = count( $grouped_data );
+    $amount_cols  = count( $grouped_data[ 0 ] );
+?>
+
 <div id="data">
-    <table class="data-output">
+    <table class="data-output<?php echo $scroll_class; ?>">
         <?php $row_counter = 1; ?>
-        <?php $amount_rows = count($grouped_data); ?>
-        <?php $amount_cols = count($grouped_data[0]); ?>
-        
         <?php foreach( $grouped_data as $row ) { ?>
             <?php if ( 1 == $row_counter ) { ?>
                 <?php include 'top-row.php'; ?>
