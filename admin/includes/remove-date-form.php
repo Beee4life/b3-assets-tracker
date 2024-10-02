3f533f8ebdc81de46c07de22f9ea901e95108b7a
Beee <info@berryplasman.com>
2024-10-03 00:42:30 +0200

    Remove checked for remove date

--- admin/includes/remove-date-form.php
+++ admin/includes/remove-date-form.php
@@ -3,7 +3,7 @@
         <select name="bp_remove_date">
             <?php echo sprintf( '<option value="">%s</option>', 'Select date' ); ?>
             <?php foreach( $all_dates as $date ) { ?>
-                <?php echo sprintf( '<option value="%s" %s>%s</option>', $date, selected($date_range, $month), bp_format_value( $date, 'date' ) ); ?>
+                <?php echo sprintf( '<option value="%s">%s</option>', $date, bp_format_value( $date, 'date' ) ); ?>
             <?php } ?>
             <option value="all">All</option>
         </select>
