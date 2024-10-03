696699a80176b40bf9ecfc60973407353f7c6f4c
Beee <info@berryplasman.com>
2024-10-03 11:43:43 +0200

    Change name

--- data.php
+++ data.php
@@ -2,7 +2,7 @@
     function b3_get_default_groups() {
         $groups = [
             1 => 'Cash',
-            2 => 'Stocks/ETF/Index Fund',
+            2 => 'Securities',
             3 => 'Collectibles',
             4 => 'Crypto',
             5 => 'Bullion',
