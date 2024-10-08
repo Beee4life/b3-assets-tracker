57a9888ce929e5f4c093fe52ab2877a21d11a38d
Beee <info@berryplasman.com>
2024-10-08 02:04:21 +0200

    Close dropdowns

--- assets/js.js
+++ assets/js.js
@@ -1,17 +1,33 @@
 jQuery(document).ready(function () {
+    var asset_group = document.getElementById('asset-groups');
     var asset_type = document.getElementById('asset-types');
-    asset_type.getElementsByClassName('anchor')[0].onclick = function(evt) {
-        if (asset_type.classList.contains('visible'))
-            asset_type.classList.remove('visible');
-        else
-            asset_type.classList.add('visible');
+
+    if ( asset_type ) {
+        asset_type.getElementsByClassName('anchor')[0].onclick = function(evt) {
+            if (asset_type.classList.contains('visible'))
+                asset_type.classList.remove('visible');
+            else
+                asset_type.classList.add('visible');
+            if (asset_group.classList.contains('visible'))
+                asset_group.classList.remove('visible');
+        }
     }
 
-    var asset_group = document.getElementById('asset-groups');
-    asset_group.getElementsByClassName('anchor')[0].onclick = function(evt) {
-        if (asset_group.classList.contains('visible'))
-            asset_group.classList.remove('visible');
-        else
-            asset_group.classList.add('visible');
+    if ( asset_group ) {
+        asset_group.getElementsByClassName('anchor')[0].onclick = function(evt) {
+            if (asset_group.classList.contains('visible'))
+                asset_group.classList.remove('visible');
+            else
+                asset_group.classList.add('visible');
+            if (asset_type.classList.contains('visible'))
+                asset_type.classList.remove('visible');
+        }
     }
+
+    jQuery('body').on('click',function(event){
+        if(!jQuery(event.target).is('.anchor')){
+            jQuery("#asset-groups").removeClass("visible");
+            jQuery("#asset-types").removeClass("visible");
+        }
+    });
 });
