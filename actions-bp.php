8c29125764b463b76751d0bfcc2d810a2aecefcd
Beee <info@berryplasman.com>
2024-10-12 19:45:58 +0200

    Remove empty string

--- actions-bp.php
+++ actions-bp.php
@@ -4,7 +4,6 @@
     function bp_add_graph( $add_graph = false ) {
         if ( $add_graph ) {
             echo '<div id="chart_div"></div>';
-            echo '';
         }
     }
     add_action( 'add_graph', 'bp_add_graph' );
