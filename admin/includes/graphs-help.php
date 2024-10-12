97ec098e6192944e613ec8b7346b5f355bb823dc
Beee <info@berryplasman.com>
2024-10-12 16:26:56 +0200

    Update help

--- admin/includes/graphs-help.php
+++ admin/includes/graphs-help.php
@@ -1,19 +1,18 @@
 <div class="graphs-help">
-    <h3>Info filters</h3>
-
     <ul>
         <li class="graph-info">
             <div class="graph-info__header">Line chart</div>
-            
+
             <ul>
                 <li>Start and end date are required</li>
+                <li>Choose either asset types or groups, not both.</li>
             </ul>
         </li>
         <li class="graph-info">
             <div class="graph-info__header">Total chart</div>
             <ul>
+                <li>Asset group/type is ignored (because the graph type defines it)</li>
                 <li>End date is required</li>
-                <li>Asset group/type is ignored (selection is made in graph type)</li>
             </ul>
         </li>
     </ul>
