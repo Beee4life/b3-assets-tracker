ce193942fda82fa1244cea126c80b252b4ffe573
Beee <info@berryplasman.com>
2024-10-12 19:44:04 +0200

    Add graph script to front

--- actions-bp.php
+++ actions-bp.php
@@ -1,7 +1,10 @@
 <?php
+    add_action( 'wp_head', 'bp_add_chart_script' );
+
     function bp_add_graph( $add_graph = false ) {
         if ( $add_graph ) {
             echo '<div id="chart_div"></div>';
+            echo '';
         }
     }
     add_action( 'add_graph', 'bp_add_graph' );
