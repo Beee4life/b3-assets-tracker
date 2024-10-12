8533fd12205dd95b887f569799f5cb6d005c5874
Beee <info@berryplasman.com>
2024-10-12 20:20:25 +0200

    Move to theme

--- actions-bp.php
+++ actions-bp.php
@@ -1,6 +1,4 @@
 <?php
-    add_action( 'wp_head', 'bp_add_chart_script' );
-
     function bp_add_graph( $add_graph = false ) {
         if ( $add_graph ) {
             echo '<div id="chart_div"></div>';
