3fafda82f8e41b866deeb48b9978c2f2e0763469
Beee <info@berryplasman.com>
2024-10-12 21:08:57 +0200

    Redirect from home

--- form-handling.php
+++ form-handling.php
@@ -11,7 +11,7 @@
                 if ( function_exists( 'bp_errors' ) ) {
                     bp_errors()->add( 'error_nonce_no_match', esc_html( __( 'Something went wrong. Please try again.', 'assets' ) ) );
                 }
-                
+
             } else {
                 if ( isset( $_POST[ 'bp_type' ] ) ) {
                     if ( empty( $_POST[ 'bp_type' ] ) ) {
@@ -32,7 +32,7 @@
                             } elseif ( '0000-00-00' !== $close_date ) {
                                 error_log('HIT else close date');
                             }
-                            
+
                             $data = [
                                 'name'        => sanitize_text_field( $_POST[ 'bp_type' ] ),
                                 'ordering'    => ! empty( $_POST[ 'bp_order' ] ) ? (int) $_POST[ 'bp_order' ] : 1,
@@ -63,7 +63,7 @@
                             $group  = isset( $_POST[ 'bp_asset_group' ] ) ? (int) $_POST[ 'bp_asset_group' ] : false;
                             $hide   = isset( $_POST[ 'bp_hide' ] ) ? $_POST[ 'bp_hide' ] : '';
                             $order  = isset( $_POST[ 'bp_order' ] ) ? (int) $_POST[ 'bp_order' ] : 1;
-                            
+
                             $data  = [
                                 'name' => $type,
                             ];
@@ -79,7 +79,7 @@
                             if ( $hide ) {
                                 $data[ 'hide' ] = $hide;
                             }
-                            
+
                             $return = $wpdb->insert( $table_types, $data );
                             if ( $return && function_exists( 'bp_errors' ) ) {
                                 bp_errors()->add( 'success_type_inserted', esc_html( __( 'Type inserted.', 'assets' ) ) );
@@ -89,7 +89,7 @@
                 }
             }
         }
-        
+
         // delete types form
         if ( isset( $_POST[ 'delete_types_nonce' ] ) ) {
             if ( ! wp_verify_nonce( $_POST[ 'delete_types_nonce' ], 'delete-types-nonce' ) ) {
@@ -107,25 +107,25 @@
                 }
             }
         }
-        
+
         // add/update data
         if ( isset( $_POST[ 'add_data_nonce' ] ) ) {
             if ( ! wp_verify_nonce( $_POST[ 'add_data_nonce' ], 'add-data-nonce' ) ) {
                 if ( function_exists( 'bp_errors' ) ) {
                     bp_errors()->add( 'error_nonce_no_match', esc_html( __( 'Something went wrong. Please try again.', 'assets' ) ) );
                 }
-                
+
             } else {
                 $validated_fields = bp_validate_form_input( $_POST );
-                
+
                 if ( true === $validated_fields ) {
                     unset( $_POST[ 'add_data_nonce' ] );
                     $input  = $_POST;
                     $values = is_array( $input[ 'bp_value' ] ) ? $input[ 'bp_value' ] : [];
-                    
+
                     if ( getenv( 'ASSETS' ) ) {
                         $assets = explode( ',', getenv( 'ASSETS' ) );
-                        
+
                         if ( ! empty( $values[ $assets[ 0 ] ] ) && ! empty( $values[ $assets[ 1 ] ] ) ) {
                             $total_degiro = $values[ $assets[ 0 ] ];
                             $total_etf    = $values[ $assets[ 1 ] ];
@@ -148,7 +148,7 @@
                             $format = [
                                 '%f'
                             ];
-                            
+
                             // check if exists
                             $query = $wpdb->prepare( "SELECT * FROM $table_data WHERE type = '%d' and date = '%s'", (int) $type, $input[ 'update_data' ] );
                             $row   = $wpdb->get_row( $query );
@@ -160,17 +160,17 @@
                                     'type'  => $type,
                                 ];
                                 $wpdb->insert( $table_data, $data );
-                                
+
                             } else {
                                 $wpdb->update( $table_data, $data, $where, $format );
                             }
 
                         }
-                        
+
                         if ( function_exists( 'bp_errors' ) ) {
                             bp_errors()->add( 'success_type_updated', esc_html( __( 'Values updated.', 'assets' ) ) );
                         }
-                        
+
                     } else {
                         // insert row
                         foreach( $values as $type => $value ) {
@@ -188,16 +188,23 @@
                 } elseif ( function_exists( 'bp_errors' ) ) {
                     bp_errors()->add( $validated_fields[ 'code' ], __( $validated_fields[ 'message' ], 'assets' ) );
                 }
+
+                if ( ! is_admin() ) {
+                    $redirect_url = get_home_url();
+                    $redirect_url = add_query_arg( 'data-updated', 'true', $redirect_url );
+                    wp_redirect( $redirect_url );
+                    exit;
+                }
             }
         }
-        
+
         // settings page
         if ( isset( $_POST[ 'assets_settings_nonce' ] ) ) {
             if ( ! wp_verify_nonce( $_POST[ 'assets_settings_nonce' ], 'assets-settings-nonce' ) ) {
                 if ( function_exists( 'bp_errors' ) ) {
                     bp_errors()->add( 'error_nonce_no_match', esc_html( __( 'Something went wrong. Please try again.', 'assets' ) ) );
                 }
-                
+
             } else {
                 if ( ! empty( $_POST[ 'bp_currency' ] ) ) {
                     update_option( 'bp_currency', $_POST[ 'bp_currency' ] );
@@ -214,12 +221,12 @@
                 }
             }
         }
-        
+
         // remove data form
         if ( isset( $_POST[ 'bp_remove_date' ] ) ) {
             global $wpdb;
             $date = $_POST[ 'bp_remove_date' ];
-            
+
             if ( 'all' === $date ) {
                 $wpdb->query( "TRUNCATE TABLE $table_data" );
 
@@ -235,7 +242,7 @@
     }
     add_action( 'init', 'process_input_forms' );
 
-    
+
     function bp_validate_form_input( $post_data = [] ) {
         if ( ! isset( $post_data[ 'bp_date' ] ) || empty( $_POST[ 'bp_date' ] ) ) {
             return [
@@ -243,6 +250,6 @@
                 'message' => esc_html( __( 'No date selected.', 'assets' ) ),
             ];
         }
-        
+
         return true;
     }
