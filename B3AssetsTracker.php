d7880f3a0c9a3b044942fad91cf076ca623af562
Beee <info@berryplasman.com>
2024-10-16 23:37:17 +0200

    Remove text doamin

--- B3AssetsTracker.php
+++ B3AssetsTracker.php
@@ -6,7 +6,6 @@
         Author:         Beee
         Author URI:     https://berryplasman.com
         License:        GPL2
-        Text domain:    b3-assets-tracker
         License:        GPL v2 (or later)
         License URI:    https://www.gnu.org/licenses/gpl-2.0.html
         Domain Path:    /languages
