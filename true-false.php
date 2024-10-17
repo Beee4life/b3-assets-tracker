b472779cac0f0ecad00eed0561dd1969c4ec12e5
Beee <info@berryplasman.com>
2024-10-18 01:15:22 +0200

    Improve true/false

--- true-false.php
+++ true-false.php
@@ -14,11 +14,9 @@
             $table = $wpdb->prefix . 'asset_types';
             $query = $wpdb->prepare( "SELECT closed FROM $table WHERE id = %d", $type );
             $results = $wpdb->get_results( $query );
-
-            if ( ! empty( $results[0]->closed ) && '0000-00-00' !== $results[0]->closed ) {
-                if ( $results[0]->closed < $dates[0] ) {
-                    return true;
-                }
+            
+            if ( ! empty( $results[ 0 ]->closed ) && '0000-00-00' !== $results[ 0 ]->closed && $results[ 0 ]->closed < $dates[ 0 ] ) {
+                return true;
             }
         }
         
@@ -28,15 +26,60 @@
     function bp_is_type_hidden( $type ) {
         if ( $type ) {
             global $wpdb;
-            $table = $wpdb->prefix . 'asset_types';
-            $query = $wpdb->prepare( "SELECT hide FROM $table WHERE id = %d", $type );
+            $table   = $wpdb->prefix . 'asset_types';
+            $query   = $wpdb->prepare( "SELECT hide FROM $table WHERE id = %d", $type );
             $results = $wpdb->get_results( $query );
-            if ( isset( $results[0]->hide ) ) {
-                if ( '1' == $results[0]->hide ) {
-                    return true;
-                }
+            
+            if ( isset( $results[0]->hide ) && '1' == $results[0]->hide ) {
+                return true;
             }
         }
         
         return false;
+    }
+    
+    
+    /**
+     * Check if a type/position has been added by date
+     *
+     * @param $type
+     * @param $data
+     *
+     * @return bool
+     */
+    function bp_is_type_added( $type, $data = [] ) {
+        if ( $type && $data ) {
+            global $wpdb;
+            $dates   = array_keys( $data );
+            $table   = $wpdb->prefix . 'asset_types';
+            $query   = $wpdb->prepare( "SELECT added FROM $table WHERE id = %d", $type );
+            $results = $wpdb->get_results( $query );
+            
+            if ( ! empty( $results[ 0 ]->added ) && '0000-00-00' !== $results[ 0 ]->added && $results[ 0 ]->added < $dates[ 0 ] ) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    /**
+     * Check if asset is hidden, closed or not started yet
+     *
+     * @param $type
+     * @param $data
+     *
+     * @return bool
+     */
+    function bp_is_visible( $type, $data = [] ) : bool {
+        $added  = bp_is_type_added( (int) $type, $data );
+        $hidden = bp_is_type_hidden( (int) $type );
+        $closed = bp_is_type_closed( (int) $type, $data );
+        
+        if ( $hidden || $closed || ! $added ) {
+            return false;
+        }
+        
+        return true;
     }
