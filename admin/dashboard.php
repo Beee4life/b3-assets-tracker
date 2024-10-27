4cf00d32ddde7ebf7c08497694414d43ec88f6cc
Beee <info@berryplasman.com>
2024-10-27 21:17:05 +0100

    Change nonce

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,20 +6,19 @@
         if ( ! current_user_can( 'manage_options' ) ) {
             wp_die( esc_html( __( 'Sorry, you do not have sufficient permissions to access this page.', 'b3-assets-tracker' ) ) );
         }
-        
+
         $all_dates   = array_values( bp_get_dates() );
         $data        = bp_get_data();
         $types       = bp_get_asset_types();
         $asset_group = [];
         $asset_type  = 'all';
-        
+
         if ( ! empty( $data ) ) {
             $dates              = array_keys( $data );
             $date_from          = $dates[ count( $dates ) - 2 ];
             $date_until         = end( $dates );
             $graph_type         = false;
             $grouped_data       = [];
-            $is_graph_page      = false;
             $is_dashboard       = true;
             $scroll_class       = false;
             $show_all_option    = true;
@@ -30,17 +29,17 @@
             $show_graph         = false;
             $show_graph_options = false;
             $show_total         = false;
-            
-            if ( isset( $_POST[ 'b3_input_nonce' ] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST[ 'b3_input_nonce' ] ) ), 'b3-input-nonce' ) ) {
+
+            if ( isset( $_POST[ 'b3_from_till_nonce' ] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST[ 'b3_from_till_nonce' ] ) ), 'b3-from-till-nonce' ) ) {
                 $asset_group = isset( $_POST[ 'asset_group' ] ) ? sanitize_text_field( wp_unslash( $_POST[ 'asset_group' ] ) ) : $asset_group;
                 $asset_type  = isset( $_POST[ 'asset_type' ] ) ? sanitize_text_field( wp_unslash( $_POST[ 'asset_type' ] ) ) : $asset_type;
-                
+
                 $date_from  = ! empty( $_POST[ 'stats_from' ] ) ? sanitize_text_field( wp_unslash( $_POST[ 'stats_from' ] ) ) : '';
                 $date_until = ! empty( $_POST[ 'stats_until' ] ) ? sanitize_text_field( wp_unslash( $_POST[ 'stats_until' ] ) ) : '';
             } else {
                 $show_diff = isset( $dates[ 1 ] ) ? true : false;
             }
-            
+
             if ( isset( $_POST[ 'add_type_nonce' ] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST[ 'add_type_nonce' ] ) ), 'add-type-nonce' ) ) {
                 if ( isset( $_POST[ 'bp_date' ] ) && isset( $_POST[ 'update_data' ] ) ) {
                     // view after update
@@ -49,7 +48,7 @@
                     // view after insert
                 }
             }
-            
+
             if ( ! empty( $date_from ) && ! empty( $date_until ) ) {
                 $grouped_data = bp_get_results_range( $date_from, $date_until, $asset_type, $asset_group, $show_all );
                 $show_diff    = true;
@@ -58,12 +57,12 @@
                 if ( 1 == count( $grouped_data ) ) {
                     $show_diff = false;
                 }
-                
+
             } else {
                 // when are there no dates ?
                $grouped_data = array_reverse( $data );
             }
-            
+
             $grouped_data = bp_process_data_for_table( $grouped_data, $show_diff, $show_total );
         }
         ?>
@@ -78,7 +77,7 @@
                 if ( function_exists( 'bp_show_error_messages' ) ) {
                     bp_show_error_messages();
                 }
-                
+
                 do_action( 'bp_admin_menu' );
             ?>
 
@@ -86,7 +85,7 @@
                 <div id="data-input">
                     <?php include 'includes/from-till-form.php'; ?>
                 </div>
-    
+
                 <div id="data-output">
                     <?php if ( $grouped_data ) { ?>
                         <?php include 'includes/data-output.php'; ?>
