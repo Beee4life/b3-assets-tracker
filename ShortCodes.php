2d4df42548988ff7fc9b6d51237d7126cefa9b66
Beee <info@berryplasman.com>
2024-10-27 18:02:08 +0100

    Make split for type

--- ShortCodes.php
+++ ShortCodes.php
@@ -11,8 +11,8 @@
             add_shortcode( 'results',   [ $this, 'shortcode_results' ] );
             add_shortcode( 'graph',     [ $this, 'shortcode_graph' ] );
         }
-        
-        
+
+
         /**
          * Shortcode to output results on front-end
          *
@@ -35,31 +35,31 @@
                     return '';
                 }
             }
-            
+
             $show_diff    = true;
             $date_from    = gmdate( 'Y-m-d', strtotime( $attributes[ 'from' ] ) );
             $date_until   = gmdate( 'Y-m-d', strtotime( $attributes[ 'till' ] ) );
             $grouped_data = bp_get_results_range( $date_from, $date_until, 'all', [] );
-            
+
             if ( 1 < count( $grouped_data ) ) {
                 $show_total   = true;
                 $grouped_data = bp_process_data_for_table( $grouped_data, $show_diff, $show_total );
                 $types        = bp_get_asset_types();
-                
+
                 if ( ! is_admin() && is_array( $grouped_data ) ) {
                     ob_start();
                     $amount_columns   = $grouped_data[ 0 ];
                     $scroll_class     = 15 < $amount_columns ? ' tablescroll' : '';
                     $shortcode_notice = sprintf( '<div class="shortcode-notice tablescroll">%s</div>', esc_html( 'Table scrolls horizontally.' ) );
-                    
+
                     if ( ! is_admin() && 6 < count( $amount_columns ) || is_admin() && 15 < count( $amount_columns ) ) {
                         printf( '<div class="shortcode-notice tablescroll">%s</div>', esc_html( 'Table scrolls horizontally.' ) );
                     }
-                    
+
                     echo '<div id="data-output"><div id="data">';
                     include 'admin/includes/data-output.php';
                     echo '</div></div>';
-                    
+
                     if ( 'true' == $attributes[ 'footer' ] ) {
                         $page_id = get_page_by_path( 'assets-tracker' );
                         if ( $page_id ) {
@@ -76,7 +76,7 @@
                         echo sprintf( '<div class="shortcode-footer">%s</div>', esc_html( $message ) );
                     }
                     $result = ob_get_clean();
-                    
+
                     return $result;
                 }
             } else {
@@ -87,8 +87,8 @@
                 }
             }
         }
-        
-        
+
+
         /**
          * Shortcode to output results on front-end
          *
@@ -106,7 +106,7 @@
                     'type'   => 'line',
                     'footer' => 'false',
                 ], $attr );
-                
+
                 if ( ( empty( $shortcode_attributes[ 'dates' ] ) && ( empty( $shortcode_attributes[ 'from' ] ) || empty( $shortcode_attributes[ 'till' ] ) ) ) || empty( $shortcode_attributes[ 'type' ] ) ) {
                     if ( current_user_can( 'manage_options' ) ) {
                         return '[shortcode is missing attributes]';
@@ -114,38 +114,43 @@
                         return '';
                     }
                 }
-                
+
                 $graph_type   = $shortcode_attributes[ 'type' ];
                 $asset_groups = [];
                 $asset_types  = 'all';
                 $grouped_data = [];
                 $show_all     = 'all' == $asset_types ? true : false;
-                $show_diff    = false;
-                
-                if ( ! empty( $shortcode_attributes[ 'dates' ] ) ) {
-                    $dates = explode( ',', $shortcode_attributes[ 'dates' ] );
-                    foreach( $dates as $date ) {
-                        $date = gmdate( 'Y-m-d', strtotime( $date ) );
-                        if ( ! array_key_exists( $date, $grouped_data ) ) {
-                            $grouped_data[ $date ] = [];
+
+                if ( 'line' === $graph_type ) {
+                    if ( ! empty( $shortcode_attributes[ 'dates' ] ) ) {
+                        $dates = explode( ',', $shortcode_attributes[ 'dates' ] );
+                        foreach( $dates as $date ) {
+                            $date = gmdate( 'Y-m-d', strtotime( $date ) );
+                            if ( ! array_key_exists( $date, $grouped_data ) ) {
+                                $grouped_data[ $date ] = [];
+                            }
+
+                            $date_rows = bp_get_data( $date );
+                            if ( ! empty( $date_rows ) ) {
+                                $grouped_data[ $date ] = $date_rows;
+                            }
                         }
 
-                        $date_rows = bp_get_data( $date );
-                        if ( ! empty( $date_rows ) ) {
-                            $grouped_data[ $date ] = $date_rows;
-                        }
+                    } elseif ( ! empty( $shortcode_attributes[ 'from' ] ) || ! empty( $shortcode_attributes[ 'till' ] ) ) {
+                        $date_from    = gmdate( 'Y-m-d', strtotime( $shortcode_attributes[ 'from' ] ) );
+                        $date_until   = gmdate( 'Y-m-d', strtotime( $shortcode_attributes[ 'till' ] ) );
+                        $grouped_data = bp_get_results_range( $date_from, $date_until, 'all', [], $show_all );
                     }
-                    
-                } elseif ( ! empty( $shortcode_attributes[ 'from' ] ) || ! empty( $shortcode_attributes[ 'till' ] ) ) {
-                    $date_from    = gmdate( 'Y-m-d', strtotime( $shortcode_attributes[ 'from' ] ) );
+
+                } elseif ( 'pie' === $graph_type ) {
                     $date_until   = gmdate( 'Y-m-d', strtotime( $shortcode_attributes[ 'till' ] ) );
-                    $show_diff    = true;
-                    $grouped_data = bp_get_results_range( $date_from, $date_until, 'all', [], $show_all );
+                    echo '<pre>'; var_dump($date_until); echo '</pre>'; exit;
+
                 }
-                
+
                 if ( 1 < count( $grouped_data ) ) {
                     $processed_data = bp_process_data_for_chart( $grouped_data, $asset_types, $asset_groups, $graph_type );
-                    
+
                     $chart_args = [
                         'asset_group' => $asset_groups,
                         'asset_type'  => $asset_types,
@@ -156,7 +161,7 @@
                     wp_localize_script( 'graphs', 'chart_vars', $chart_args );
 
                     return bp_get_chart_element();
-                    
+
                 } else {
                     if ( current_user_can( 'manage_options' ) ) {
                         return '<p>Er is te weinig data om weer te geven. Check de shortcode in de content.</p>';
