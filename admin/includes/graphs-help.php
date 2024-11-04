34b24ae493ccfc85b7f50048c75ac9070da574c8
Beee <info@berryplasman.com>
2024-11-04 20:11:55 +0100

    Add more translatable strings

--- admin/includes/graphs-help.php
+++ admin/includes/graphs-help.php
@@ -1,18 +1,30 @@
 <div class="graphs-help">
     <ul>
         <li class="graph-info">
-            <div class="graph-info__header">Line chart</div>
+            <div class="graph-info__header">
+                <?php esc_html_e( 'Line chart', 'b3-assets-tracker' ); ?>
+            </div>
 
             <ul>
-                <li>Choose either asset types or groups, not both.</li>
-                <li>Start and end date are required</li>
+                <li>
+                    <?php esc_html_e( 'Choose either asset types or groups, not both.', 'b3-assets-tracker' ); ?>
+                </li>
+                <li>
+                    <?php esc_html_e( 'Start and end date are required.', 'b3-assets-tracker' ); ?>
+                </li>
             </ul>
         </li>
         <li class="graph-info">
-            <div class="graph-info__header">Total chart</div>
+            <div class="graph-info__header">
+                <?php esc_html_e( 'Pie chart', 'b3-assets-tracker' ); ?>
+            </div>
             <ul>
-                <li>Asset group/type is ignored (because it's defines by graph type)</li>
-                <li>End date is required</li>
+                <li>
+                    <?php esc_html_e( "Asset group/type is ignored (because it's defines by graph type).", 'b3-assets-tracker' ); ?>
+                </li>
+                <li>
+                    <?php esc_html_e( 'End date is required.', 'b3-assets-tracker' ); ?>
+                </li>
             </ul>
         </li>
     </ul>
