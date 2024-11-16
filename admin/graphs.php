504dd044e7e3cb640c9097799981f8765bd7ea43
Beee <info@berryplasman.com>
2024-11-16 19:56:39 +0100

    Select dates after submit

--- admin/graphs.php
+++ admin/graphs.php
@@ -26,6 +26,7 @@
         $last_date             = end( $dates );
         $selected_asset_types  = [];
         $selected_asset_groups = [];
+        $selected_dates        = [];
         $show_asset_groups     = true;
         $show_asset_types      = true;
         $show_all_option       = false;
@@ -38,6 +39,7 @@
             $graph_type            = isset( $_POST[ 'graph_type' ] ) ? sanitize_text_field( wp_unslash( $_POST[ 'graph_type' ] ) ) : $graph_type;
             $selected_asset_types  = isset( $_POST[ 'asset_type' ] ) ? wp_unslash( $_POST[ 'asset_type' ] ) : $selected_asset_types;
             $selected_asset_groups = isset( $_POST[ 'asset_group' ] ) ? wp_unslash( $_POST[ 'asset_group' ] ) : $selected_asset_groups;
+            $selected_dates        = isset( $_POST[ 'bp_dates' ] ) ? wp_unslash( $_POST[ 'bp_dates' ] ) : [];
 
             if ( is_array( $selected_asset_types ) && 1 < count( $selected_asset_types ) && in_array( 'all', $selected_asset_types ) ) {
                 // empty types on error
