6a8268403871b770e919ec4840fe5c8c5c221b05
Beee <info@berryplasman.com>
2024-11-16 19:57:00 +0100

    Add dates dropdown

--- assets/js.js
+++ assets/js.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function () {
     var asset_group = document.getElementById('asset-groups');
     var asset_type = document.getElementById('asset-types');
+    var dates = document.getElementById('dates');
 
     if ( asset_type ) {
         asset_type.getElementsByClassName('anchor')[0].onclick = function(evt) {
@@ -21,6 +22,15 @@ jQuery(document).ready(function () {
                 asset_group.classList.add('visible');
             if (asset_type.classList.contains('visible'))
                 asset_type.classList.remove('visible');
+        }
+    }
+
+    if ( dates ) {
+        dates.getElementsByClassName('anchor')[0].onclick = function(evt) {
+            if (dates.classList.contains('visible'))
+                dates.classList.remove('visible');
+            else
+                dates.classList.add('visible');
         }
     }
 });
