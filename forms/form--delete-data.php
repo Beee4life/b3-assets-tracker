1d9be1a3a9c59f472e0f3aaf2a620dd597974cf3
Beee <info@berryplasman.com>
2024-11-24 21:07:56 +0100

    Remove duplicate

--- forms/form--delete-data.php
+++ forms/form--delete-data.php
@@ -30,26 +30,5 @@
                 }
             }
         }
-
-        if ( isset( $_POST[ 'delete_types_nonce' ] ) ) {
-            if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST[ 'delete_types_nonce' ] ) ), 'delete-types-nonce' ) ) {
-                if ( function_exists( 'bp_errors' ) ) {
-                    bp_errors()->add( 'error_nonce_no_match', esc_html( __( 'Something went wrong. Please try again.', 'b3-assets-tracker' ) ) );
-                    return;
-                }
-
-            } else {
-                global $wpdb;
-                $table_data = $wpdb->prefix . 'asset_types';
-
-                if ( isset( $_POST[ 'bp_delete_type' ] ) && is_array( $_POST[ 'bp_delete_type' ] ) && ! empty( $_POST[ 'bp_delete_type' ] ) ) {
-                    $types = wp_unslash( $_POST[ 'bp_delete_type' ] );
-                    foreach( $types as $type ) {
-                        $wpdb->delete( $table_types, array( 'id' => $type ), array( '%d' ) );
-                        $wpdb->delete( $table_data, array( 'type' => $type ), array( '%d' ) );
-                    }
-                }
-            }
-        }
     }
     add_action( 'init', 'yoft_delete_data_form' );
